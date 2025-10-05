f866941030e8dda8a5bb36ef26b0279f8bca52e3
prathmesh khandare <prathameshkhandare15@gmail.com>
2025-10-05 16:49:27 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { ExternalLink, Github } from "lucide-react";
-import  Button  from "@/components/ui/Button";
+import  Button  from "@/components/ui/button";
 
 export default function Projects() {
   const projects = [
@@ -170,3 +170,4 @@ export default function Projects() {
   );
 
 }
+
